9cc2ce022a5f1dacfab294f07ab2fbce5bce2022
Judson James <judsonjames96@gmail.com>
2024-10-22 14:43:17 -0400

    Initial commit 'CounterSubsystem'

---
+++ 2024/simple-counter-subsystem/src/main/java/frc/robot/subsystems/counter/CounterSubsystem.java
@@ -0,0 +1,58 @@
+package frc.robot.subsystems.counter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import org.littletonrobotics.junction.Logger;
+
+public class CounterSubsystem extends SubsystemBase {
+    private int counter = 0;
+    private int step = 0;
+
+    private int upperBound = 50;
+    private int lowerBound = 0;
+
+    public CounterSubsystem() {
+        this.step = 1;
+    }
+
+    public CounterSubsystem(int step) {
+        this.step = step;
+    }
+
+    @Override
+    public void periodic() {
+        Logger.recordOutput("Counter/Value", this.counter);
+    }
+
+    public Command incrementCounter() {
+        return new RunCommand(
+                () -> {
+                    counter += step;
+                });
+    }
+
+    public Command decreaseCounter() {
+        return new RunCommand(
+                () -> {
+                    counter -= step;
+                });
+    }
+
+    public Command setCounterToValue(int value) {
+        return new InstantCommand(
+                () -> {
+                    counter = value;
+                });
+    }
+
+    public Trigger isBelowUpperLimit() {
+        return new Trigger(() -> this.counter < this.upperBound);
+    }
+
+    public Trigger isAboveLowerLimit() {
+        return new Trigger(() -> this.counter > this.lowerBound);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,8 @@
+# ROBOTZ Garage Programming Learning Exercises
+
+All examples are pulled from AdvantageKit's sample projects. Credit to bases goes to:
+https://github.com/Mechanical-Advantage/AdvantageKit
+
+When cloning this repository, ensure that you have already installed WPILib and that you have a development environment for WPILib. This organization assumes you use VSCode + WPILib extensions.
+
+Open the project by year, and open each exercise in separate VSCode windows if possible. Each exercise is its own project.
