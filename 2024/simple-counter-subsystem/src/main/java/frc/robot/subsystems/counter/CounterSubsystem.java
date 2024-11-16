9943607ed096faa2ce716e67273fdb81b5342ffb
Josh <jjenki012308@yahoo.com>
2024-11-15 19:01:04 -0800

    y

--- 2024/simple-counter-subsystem/src/main/java/frc/robot/subsystems/counter/CounterSubsystem.java
+++ 2024/simple-counter-subsystem/src/main/java/frc/robot/subsystems/counter/CounterSubsystem.java
@@ -8,51 +8,51 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import org.littletonrobotics.junction.Logger;
 
 public class CounterSubsystem extends SubsystemBase {
-    private int counter = 0;
-    private int step = 0;
+  private int counter = 0;
+  private int step = 0;
 
-    private int upperBound = 50;
-    private int lowerBound = 0;
+  private int upperBound = 50;
+  private int lowerBound = 0;
 
-    public CounterSubsystem() {
-        this.step = 1;
-    }
+  public CounterSubsystem() {
+    this.step = 1;
+  }
 
-    public CounterSubsystem(int step) {
-        this.step = step;
-    }
+  public CounterSubsystem(int step) {
+    this.step = step;
+  }
 
-    @Override
-    public void periodic() {
-        Logger.recordOutput("Counter/Value", this.counter);
-    }
+  @Override
+  public void periodic() {
+    Logger.recordOutput("Counter/Value", this.counter);
+  }
 
-    public Command incrementCounter() {
-        return new RunCommand(
-                () -> {
-                    counter += step;
-                });
-    }
+  public Command incrementCounter() {
+    return new RunCommand(
+        () -> {
+          counter += step;
+        });
+  }
 
-    public Command decreaseCounter() {
-        return new RunCommand(
-                () -> {
-                    counter -= step;
-                });
-    }
+  public Command decreaseCounter() {
+    return new RunCommand(
+        () -> {
+          counter -= step;
+        });
+  }
 
-    public Command setCounterToValue(int value) {
-        return new InstantCommand(
-                () -> {
-                    counter = value;
-                });
-    }
+  public Command setCounterToValue(int value) {
+    return new InstantCommand(
+        () -> {
+          counter = value;
+        });
+  }
 
-    public Trigger isBelowUpperLimit() {
-        return new Trigger(() -> this.counter < this.upperBound);
-    }
+  public Trigger isBelowUpperLimit() {
+    return new Trigger(() -> this.counter < this.upperBound);
+  }
 
-    public Trigger isAboveLowerLimit() {
-        return new Trigger(() -> this.counter > this.lowerBound);
-    }
+  public Trigger isAboveLowerLimit() {
+    return new Trigger(() -> this.counter > this.lowerBound);
+  }
 }
